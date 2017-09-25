43c7846f2a69a4dc25aeb85ebcb72a10281cf3b4
Emil Edberg <edem16@nti.umea>
2017-09-25 09:56:13 +0200

    Uppg 3.1 3.2

---
+++ Kapitel3.java
@@ -0,0 +1,13 @@
+public class Kapitel3{
+	public static void main (String [] args){
+		int i1, i2;
+		double d1, d2;
+		byte b1, b2;
+		i1 = 500;
+		d1 = 500;
+		b1 = 500;
+		i2 = 500.7;
+		d2 = 500.7;
+		b2 = 500.7;
+	}
+}
