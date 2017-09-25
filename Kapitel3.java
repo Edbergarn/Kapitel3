1d5c01f4e03ef7dd5aee96f2f09eef55b50dabc7
Emil Edberg <edem16@nti.umea>
2017-09-25 10:53:30 +0200

    Uppg 3.4

--- Kapitel3.java
+++ Kapitel3.java
@@ -10,6 +10,7 @@ public class Kapitel3{
 		char a1 = '\u00E5'; //å
 		char a2 = '\u00E4'; //ä
 		char o1 = '\u00F6'; //ö
+		/*
 		double pi = 3.1415926536;
 		double area;
 
@@ -17,7 +18,13 @@ public class Kapitel3{
 		double ra = Double.parseDouble(r);
 		area = ra * ra * pi;
 		double Omkrets = ra * 2 * pi;
-		JOptionPane.showMessageDialog(null, Omkrets + "\n" + area);
+		JOptionPane.showMessageDialog(null, Omkrets + "\n" + area);*/
+		String minuter = JOptionPane.showInputDialog(null, "Antal minuter du ringer i m" + a1 +"naden" );
+		String priset = JOptionPane.showInputDialog(null, "Hur mycket " + a2 +"r minutpriset?");
+		double m = Double.parseDouble(minuter);
+		double p = Double.parseDouble(priset);
+		double summa = m * p;
+		JOptionPane.showMessageDialog(null, summa);
 
 	}
 }
